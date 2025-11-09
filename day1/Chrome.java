ed1df2c48f97f424c31ae5d88aca795d79c4fea0
snehaamurali <90207196+snehaamurali@users.noreply.github.com>
2025-11-08 21:41:19 -0500

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		 // Declaring and initializing variables of various data types
+        float version = 91.0f;                 // float
+        String developer = "Google";           // String (non-primitive)
+        boolean isBeta = false;                // boolean
+        int releaseYear = 2008;                // int
+        char shortcutKey = 'C';                // char
+        
+        // Printing variable values to console
+        System.out.println("Chrome Version: " + version);
+        System.out.println("Developer: " + developer);
+        System.out.println("Is Beta Version: " + isBeta);
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Shortcut Key: " + shortcutKey);
+	}
+
+}
