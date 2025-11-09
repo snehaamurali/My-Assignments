d893a8ea858db3aea4bb658d75b90c56d8a23cd6
snehaamurali <90207196+snehaamurali@users.noreply.github.com>
2025-11-08 21:32:17 -0500

    Add files via upload
    day1

---
+++ day1/FirstProgram.java
@@ -0,0 +1,11 @@
+package week1.day1;
+//Access modifier  keyword
+public class FirstProgram {
+
+	public static void main(String[] args) {
+    System.out.println("Here is my First Program");
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ day1/LearnDatatypes.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		//byte
+		byte age=30;
+		//short
+		short ageage=128;
+		
+
+	}
+
+}
