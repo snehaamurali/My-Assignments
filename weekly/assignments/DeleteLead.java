37174889ebc5495c6c0edca7ef31f95579fcdcd1
snehaamurali <90207196+snehaamurali@users.noreply.github.com>
2025-12-01 00:14:58 -0500

    Add files via upload

---
+++ weekly/assignments/DeleteLead.java
@@ -0,0 +1,10 @@
+package weekly.assignments;
+
+public class DeleteLead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- weekly/assignments/MissingElement.java
+++ weekly/assignments/MissingElement.java
@@ -24,4 +24,4 @@ package weekly.assignments;
 	    }
 	}
 
-}
+
