82942de76c877f41092469762bb0ae57dd9208a1
snehaamurali <90207196+snehaamurali@users.noreply.github.com>
2025-12-19 14:03:44 -0500

    Add files via upload

---
+++ week6/day1/LearnTwoDimensionalArray.java
@@ -0,0 +1,15 @@
+package week6.day1;
+
+public class LearnTwoDimensionalArray {
+ public static void main(String[] args) {
+	//{{"Testleaf","Vineeth","Rajendran"},{"Qeagle","Hari","R"}}
+	 String arr[]=new String [5];
+	 String[][] data=new String[2][3];
+	 data[0][0]="Testleaf"
+	 data[0][1]="Vineeth"
+	 data[0][2]="Rajendran"
+	 data[1][0]="Qeagle"
+	 data[1][1]="Hari"
+	 data[1][2]="R"
+}
+}
